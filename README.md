4fcc81d7aded34398035d2e9bc1c11dafa4ebd1f
mingtianly <starly@163.com>
2020-11-27 09:54:55 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## 后台管理系统
+[starly@163.com](https://mail.163.com/)
---
+++ application/index/controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+        echo 'hello word!';
+    }
+
+    public function hello($name = 'ThinkPHP5')
+    {
+        return 'hello,' . $name;
+    }
+}
